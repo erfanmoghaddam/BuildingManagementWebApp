5f3372b7706c7bef56f455efddf0449a1ff7785c
erfanmoghaddam <74089467+erfanmoghaddam@users.noreply.github.com>
2021-02-05 01:02:50 -0500

    Add files via upload
    added models

---
+++ BuildingManagementWebApp/Models/PartialClasses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace BuildingManagementWebApp.Models
+{
+    [MetadataType(typeof(Metadata))]
+    public partial class Employee
+    {
+    }
+
+    public partial class Tenant
+    {
+    }
+}
